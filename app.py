e661125d2a839874957db10733f923281107aa3e
fadefa88 <62143875+fadefa88@users.noreply.github.com>
2024-07-13 17:52:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is my custom docker application! Aggiornato!'
+    return render_template('index.html')
